9617c4130ff5f29b371cea876f72619c5d4d60f2
Anas-kaithakoden <anaskaithakoden@gmail.com>
2026-07-09 00:42:33 +0530

    learned routing and paths

--- fast_api/main.py
+++ fast_api/main.py
@@ -8,3 +8,28 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message":"hello world"}
+
+@app.get("/posts")
+def list_posts():
+    return [
+        {
+            "id": 1,
+            "title": "FastAPI Basics"
+        },
+        {
+            "id": 2,
+            "title": "SQLAlchemy"
+        }
+    ]
+
+@app.get("/posts/{post_id}")
+def return_posts(post_id : int):
+    return {
+        "post_id" : 5
+    }
+
+@app.get("/posts?limit=10")
+def send_posts(limit: int = 10):
+    return {
+    "limit": 10
+}
